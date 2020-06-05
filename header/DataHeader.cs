394cb360b08e00c2696b0fedf997ac958ea9d183
Nishantthummar <59600723+Nishantthummar@users.noreply.github.com>
2020-06-05 19:22:49 +0530

    header file
    15 byte stream

---
+++ header/DataHeader.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Json;
+
+namespace header
+{
+    public class DataHeader
+    {
+        public byte dataType;
+        public byte sensorId;
+        public byte timeStampuSec; // 4 byte
+        public byte timeStampHrs;  // 6 bit
+        public byte sampleCountIncre; // 5 bit
+        public byte reserved1; // 1 bit
+        public byte sampleCount; // 3 byte
+        public byte timeDeltaUnit; // 4 bit
+        public byte timeDelta; // 12 bit
+        public byte dataFormat; // 4 bit
+        public byte fixedPointM; // 5 bit
+        public byte reserved2; // 1 bit
+        public byte dataCount; // 10 bit
+
+         public T serializ<T>(byte[] data)
+        {
+            if (data == null)
+                return default(T);
+            BinaryFormatter bf = new BinaryFormatter();
+            using (MemoryStream ms = new MemoryStream(data))
+            {
+                object obj = bf.Deserialize(ms);
+                return (T)obj;
+            }
+        }
+        
+       
+        /*public DataHeader (byte[] data)
+        {
+            if (data == null) { }
+                //default(DataHeader);
+            BinaryFormatter bf = new BinaryFormatter();
+            using(MemoryStream ms = new MemoryStream(data))
+            {
+                object obj = bf.Deserialize(ms);
+                
+            }
+
+            
+        }*/
+
+
+
+
+
+    }
+}
---
+++ header/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Json;
+using ImTools;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Json;
+using System.Security.Cryptography;
+
+namespace header
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+            public byte[] data = { 0X00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F };
+            head h1 = new head();
+            h1.serializ<DataHeader>(data);
+     }
+   
+ 
+  
+}
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+}
